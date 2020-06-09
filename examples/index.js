ec3e157278fd51b31af6910ffed53aa6c7613993
유영준 <isaac_you@mhand.net>
2020-06-10 02:35:42 +0900

    * result 파일 저장 형식으로 수정

--- examples/index.js
+++ examples/index.js
@@ -1,4 +1,15 @@
 const generator = require('../functions/generator');
 
 var myArgs = process.argv.slice(2);
-console.log(generator.generate(myArgs[0]));
+// console.log(generator.generate(myArgs[0]));
+
+
+const fs = require('fs');
+saveQuizsToNewFiles(myArgs[0]);
+
+async function saveQuizsToNewFiles(category) {
+  for(var i = 0; i < 3000; i++) {
+    const result = JSON.stringify(generator.generate(category));
+    await fs.writeFileSync(`results/${myArgs[0]}/${i}.json`, result);
+  }
+}
